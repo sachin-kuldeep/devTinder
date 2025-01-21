efeca02e7f0a64b32f9d14074df5f871c646c32a
Sachin Kuldeep <sachinkuldeep201@gmail.com>
2025-01-22 00:49:21 +0530

    About routes

--- src/app.js
+++ src/app.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const app = express();
 
-app.use('/', (req, res)=>{
-    res.send('Hello from the Dashboard!');
-});
-
-app.use('/hello', (req, res)=>{
-    res.send('Hello hello hello!');
+//this will only handle the GET call to /user
+app.get('/user/:userId/:name/:password', (req, res)=>{
+    console.log(req.params);
+    res.send({name: 'sachin'});
 });
 
 app.listen(7777, ()=>{
