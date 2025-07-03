527625b87bc7a81b485e43d69aede314a876dffb
Sachin Kuldeep <sachinkuldeep201@gmail.com>
2025-07-03 13:37:35 +0530

    Allowed CORS to Vercel

--- src/app.js
+++ src/app.js
@@ -8,12 +8,23 @@ const http  = require("http");
 
 require('dotenv').config();
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://dev-tinder-web-lake.vercel.app', // your deployed frontend
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+}));
+
+
 app.use(express.json());
 app.use(cookieParser());
 
