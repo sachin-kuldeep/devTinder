aeb758698d9bb17907fe709920b77dc697436bab
Sachin Kuldeep <sachinkuldeep201@gmail.com>
2025-07-05 13:47:36 +0530

    allowed vercel origin to socket

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -13,7 +13,8 @@ const getSecretRoomId = (userId, targetUserId) => {
 const initializeSocket = (server) => {
   const io = socket(server, {
     cors: {
-      origin: "http://localhost:5173",
+      origin: ["http://localhost:5173", "dev-tinder-web-lake.vercel.app"],
+      credentials: true,
     },
   });
 
